5ffe89aabfb5285159241d0c2498ecc7f5719711
Syed-rayyan12 <srayyan314@gmail.com>
2026-02-12 20:20:47 +0500

    code updated

--- frontend/components/landing-page/footer.tsx
+++ frontend/components/landing-page/footer.tsx
@@ -8,7 +8,8 @@ export default function Footer() {
       style={{
         backgroundImage: 'url(/images/footer.png)',
         width: '100%',
-        minHeight: '60vh'
+        minHeight: '60vh',
+        padding:'100px 0px 0px'
       }}
     >
       <div className="max-w-7xl pt-40 md:pt-32 lg:pt-80 xl:pt-60 mx-auto px-4 md:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8 text-white">

f82ba09cbaa363176c73bb5acc23cab8c320915e
Syed-rayyan12 <srayyan314@gmail.com>
2026-02-13 16:36:25 +0500

    code updated

--- frontend/components/landing-page/product-grid.tsx
+++ frontend/components/landing-page/product-grid.tsx
@@ -236,7 +236,7 @@ export default function NurseriesPage() {
 
 
         {/* GRID */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 md:gap-8 pb-20 md:pb-40 bg-white">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 md:gap-22 pb-20 md:pb-40 bg-white">
           {loading ? (
             <div className="col-span-2 text-center py-20">
               <p className="text-gray-500 text-lg">Loading nurseries...</p>
